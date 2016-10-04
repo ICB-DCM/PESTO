1fa4ec5a447a002c82a1ece88627e3301d11d1ba
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2016-10-04 14:42:57 +0200

    Use PestoOptions class for getPropertyMultiStarts.m

--- getPropertyMultiStarts.m
+++ getPropertyMultiStarts.m
@@ -1,4 +1,4 @@
-function [properties,fh] = getPropertyMultiStarts(varargin)
+function [properties,fh] = getPropertyMultiStarts(properties, parameters, varargin)
 % getPropertyMultiStarts.m evaluates the properties for the different
 %   mutli-start results.
 %
@@ -9,96 +9,54 @@ function [properties,fh] = getPropertyMultiStarts(varargin)
 %
 % Parameters:
 % varargin:
-% properties: property struct containing at least:<pre>
-%   .number ... number of parameter
-%   .min ... lower bound for property values       
-%   .max ... upper bound for property values       
-%   .name = {'name1',...} ... names of the parameters       
-%   .function = {'function1',...} ... functions to evaluate property  
+% properties: property struct containing at least:
+%    * .number: Number of properties
+%    * .min: lower bound for property values       
+%    * .max: upper bound for property values       
+%    * .name = {'name1',...}: names of the properties 
+%    * .function = {'function1',...}: functions to evaluate property  
 %       values. These functions provide the values of the respective  
 %       properties and the corresponding 1st and 2nd order
-%       derivatives.</pre>
-% parameters: parameter struct containing at least:<pre>
-%   .MS ... information about multi-start optimization
-%       .par(*,i) ... ith MAP
-%       .logPost(i) ... log-posterior for ith MAP
-%       .gradient(*,i) ... gradient of log-posterior at ith MAP
-%       .hessian(*,*,i) ... hessian of log-posterior at ith MAP
-%       .exitflag ... exitflag the optimizer returned
-%   Note: This struct is obtained using getMultiStarts.m.</pre>
-% options: options of algorithm<pre>
-%   .comp_type ... type of computations
-%       = 'sequential' (default) ... classical sequential (in core) method
-%       = 'parallel' ... multi-core method exploiting parfor
-%   .mode ... output of algorithm
-%       = 'visual' (default) ... plots are gnerated which show the progress
-%       = 'text' ... optimization results for multi-start is printed on screen
-%       = 'silent' ... no output during the multi-start local optimization
-%   .fh ... handle of figure in which results are printed. If no
-%       handle is provided, a new figure is used.
-%   .save ... determine whether results are directly saved
-%       = false (default) ... results are not saved
-%       = true ... results are stored do an extra folder
-%   .foldername ... name of the folder in which results are stored.
-%       If no folder is provided, a random foldername is generated.</pre>
+%       derivatives.
+% parameters: parameter struct containing at least:
+%   * .MS: information about multi-start optimization
+%   * .par(*,i): ith MAP
+%   * .logPost(i): log-posterior for ith MAP
+%   * .gradient(*,i): gradient of log-posterior at ith MAP
+%   * .hessian(*,*,i): hessian of log-posterior at ith MAP
+%   * .exitflag: exitflag the optimizer returned
+%   Note: This struct is obtained using getMultiStarts.m.
+% options: A PestoOptions object holding various options for the algorithm.
 %
 % Return values:
-% properties: updated parameter object containing:<pre>
-%   .MS ... properties for multi-start optimization results
-%       .par(*,i) ... ith MAP
-%       .logPost(i) ... log-posterior for ith MAP
-%       .exitflag(i) ... exit flag of ith MAP
-%       .prop(j,i) ... values of jth property for ith MAP
-%       .prop_Sigma(*,*,i) ... covariance of properties for ith MAP</pre>
+% properties: updated parameter object containing:
+%   * .MS: properties for multi-start optimization results
+%   * .par(:,i): ith MAP
+%   * .logPost(i): log-posterior for ith MAP
+%   * .exitflag(i): exit flag of ith MAP
+%   * .prop(j,i): values of jth property for ith MAP
+%   * .prop_Sigma(:,:,i): covariance of properties for ith MAP
 % fh: figure handle
 %
 % History:
 % * 2015/03/03 Jan Hasenauer
+% * 2016/04/10 Daniel Weindl
 
 %% Check and assign inputs
-if nargin >= 2
-    properties = varargin{1};
-    parameters = varargin{2};
+if nargin >= 1
+    options = varargin{1};
+    if ~isa(options, 'PestoOptions')
+        error('Third argument is not of type PestoOptions.')
+    end
 else
-    error('getPropertyMultiStarts requires at least two inputs.')
+    options = PestoOptions();
 end
 
-% Check properties:
-if ~isfield(properties,'min') || ~isfield(properties,'max')
-    error('Algorithm requires lower and upper bounds');
-else
-    properties.min = properties.min(:);
-    properties.max = properties.max(:);
-end
-if length(properties.min) ~= length(properties.max)
-	error('Dimension of properties.min and properties.max does not agree.');
-else
-    if max(properties.min >= properties.max)
-        error('There exists at least one i for which properties.min(i) >= properties.max(i).');
-    end
-end
-if ~isfield(properties,'number')
-    properties.number = length(properties.min);
-else
-    if properties.number ~= length(properties.min)
-        error('Dimension mismatch: properties.number ~= length(properties.min).');
-    end
-end
-
+properties = propertySanityCheck(properties);
 
 % Check initial guess
 if ~isfield(parameters,'guess')
     parameters.guess = [];
-end
-
-% Check and assign options
-options.comp_type = 'sequential'; % 'parallel';
-options.mode = 'visual'; % 'text','silent'
-options.save = false; % true
-options.foldername = strrep(datestr(now,31),' ','__');
-options.fh = [];
-if nargin == 3
-    options = setdefault(varargin{3},options);
 end
 
 %% Initialization and figure generation
