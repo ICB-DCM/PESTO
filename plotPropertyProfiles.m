c04edf51804b84ecdf501c1565ed3dac4160d800
daniel.weindl <daniel.weindl@helmholtz-muenchen.de>
2016-10-21 13:02:20 +0200

    Remove unnecessary additional parameter checking before plot*Uncertainty(). Consider removing these function completely.

--- plotPropertyProfiles.m
+++ plotPropertyProfiles.m
@@ -28,45 +28,18 @@ function fh = plotPropertyProfiles(properties, varargin)
 % * 2015/03/02 Jan Hasenauer
 % * 2016/10/10 Daniel Weindl
 
-%% Check and assign inputs
-% Plot type
-type = '1D';
-if length(varargin) >= 1 && ~isempty(varargin{1})
-    type = varargin{1};
-    if ~max(strcmp({'1D','2D'},type))
-        error('The ''type'' of plot is unknown.')
-    end
+switch length(varargin) 
+    case 0
+        fh = plotPropertyUncertainty(properties);
+    case 1
+        fh = plotPropertyUncertainty(properties, varargin{1});
+    case 2
+        fh = plotPropertyUncertainty(properties, varargin{1}, varargin{2});
+    case 3
+        fh = plotPropertyUncertainty(properties, varargin{1}, varargin{2}, varargin{3});
+    case 4
+        fh = plotPropertyUncertainty(properties, varargin{1}, varargin{2}, varargin{3}, varargin{4});
+    otherwise
+        error('Too many arguments for plotPropertyProfiles().');
 end
-
-% Open figure
-if length(varargin) >= 2 && ~isempty(varargin{2})
-        fh = figure(varargin{2});
-else
-    fh = figure;
 end
-
-% Index of subplot which is updated
-I = 1:properties.number;
-if length(varargin) >= 3
-    if ~isempty(varargin{3})
-        I = varargin{3};
-        if ~isnumeric(I) || max(abs(I - round(I)) > 0)
-            error('I is not an integer vector.');
-        end
-    end
-end
-
-% Options
-% General plot options
-options = PestoPlottingOptions();
-
-% Assignment of user-provided options
-if length(varargin) >= 4
-    if ~isa(varargin{4}, 'PestoPlottingOptions')
-        error('Argument 4 is not of type PestoPlottingOptions.')
-    end
-    options = setdefault(varargin{4},options);
-end
-
-%% Call plotUncertainty.m
-fh = plotPropertyUncertainty(properties,type,fh,I,options);
