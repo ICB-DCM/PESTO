8cdb9f7ec827d830f4d2c273cdf48d512d5e007d
daniel.weindl <daniel.weindl@helmholtz-muenchen.de>
2017-02-26 16:07:38 +0100

    Added more tests based on the conversion reaction example

--- tests/ConversionReactionTest.m
+++ tests/ConversionReactionTest.m
@@ -11,7 +11,6 @@ classdef ConversionReactionTest < matlab.unittest.TestCase
         y = [0.0244; 0.0842; 0.1208; 0.1724; 0.2315; 0.2634; ...
             0.2831; 0.3084; 0.3079; 0.3097; 0.3324]; % Measurement data
         objectiveFunction
-        optionsMultistart
     end
     
     methods(TestMethodSetup)
@@ -30,15 +29,7 @@ classdef ConversionReactionTest < matlab.unittest.TestCase
         function setupObjectiveFunction(testCase)
             testCase.objectiveFunction = @(theta) logLikelihoodCR(theta, testCase.t, testCase.y, testCase.sigma2, 'log');
         end
-        
-        function setupMultiStartOptions(testCase)
-            testCase.optionsMultistart = PestoOptions();
-            testCase.optionsMultistart.obj_type = 'log-posterior';
-            testCase.optionsMultistart.n_starts = 4;
-            testCase.optionsMultistart.comp_type = 'sequential';
-            testCase.optionsMultistart.mode = 'silent';
-        end
-        
+                
     end
     
     methods(TestMethodTeardown)
@@ -51,29 +42,114 @@ classdef ConversionReactionTest < matlab.unittest.TestCase
     methods (Test)
         function testObjectiveFunction(testCase)
             actLLH = testCase.objectiveFunction(testCase.theta_true);
-            testCase.fatalAssertEqual(actLLH, testCase.llh_true, 'AbsTol', eps)
+            testCase.fatalAssertEqual(actLLH, testCase.llh_true, 'AbsTol', eps, 'Likelihood function result is wrong.')
         end
         
-        function testGetMultiStarts(testCase)
-            numStarts = testCase.optionsMultistart.n_starts;
-            numParams = testCase.parameters.number;
-            multiStartParams = getMultiStarts(testCase.parameters, testCase.objectiveFunction, testCase.optionsMultistart);
+        function testGetMultiStartsFminconSingleStart(testCase)
+            optionsMS = PestoOptions();
+            optionsMS.obj_type = 'log-posterior';
+            optionsMS.n_starts = 1;
+            optionsMS.comp_type = 'sequential';
+            optionsMS.mode = 'silent';
 
-            testCase.assertEqual(multiStartParams.MS.n_starts, testCase.optionsMultistart.n_starts);
+            multiStartParams = getMultiStarts(testCase.parameters, testCase.objectiveFunction, optionsMS);
+            testCase.verifyMultiStartResults(multiStartParams, optionsMS);
+        end
+        
+        function testGetMultiStartsFmincon(testCase)
+            optionsMultistart = PestoOptions();
+            optionsMultistart.obj_type = 'log-posterior';
+            optionsMultistart.n_starts = 4;
+            optionsMultistart.comp_type = 'sequential';
+            optionsMultistart.mode = 'silent';
+
+            multiStartParams = getMultiStarts(testCase.parameters, testCase.objectiveFunction, optionsMultistart);
+            testCase.verifyMultiStartResults(multiStartParams, optionsMultistart);
+        end
+        
+        function testGetMultiStartsMeigo(testCase)
+            testCase.assumeTrue(exist('MEIGO', 'file') == 2,'MEIGO not in path.');
             
-            testCase.verifySize(multiStartParams.MS.par0, [numParams, numStarts]);
-            testCase.verifySize(multiStartParams.MS.par, [numParams, numStarts]);
-            testCase.verifySize(multiStartParams.MS.logPost0, [numStarts, 1]);
-            testCase.verifySize(multiStartParams.MS.logPost, [numStarts, 1]);
-            testCase.verifySize(multiStartParams.MS.gradient, [numParams, numStarts]);
-            testCase.verifySize(multiStartParams.MS.hessian, [numParams, numParams, numStarts]);
-            testCase.verifySize(multiStartParams.MS.n_objfun, [numStarts, 1]);
-            testCase.verifySize(multiStartParams.MS.n_iter, [numStarts, 1]);
-            testCase.verifySize(multiStartParams.MS.t_cpu, [numStarts, 1]);
-            testCase.verifySize(multiStartParams.MS.exitflag, [numStarts, 1]);
+            MeigoOptions = struct(...
+                'maxeval', 1e3, ...
+                'iterprint', 0, ...
+                'local', struct('solver', 'fmincon', ...
+                'finish', 'fmincon', ...
+                'iterprint', 0) ...
+                );
+            
+            optionsMultistartMeigo = PestoOptions();
+            optionsMultistartMeigo.obj_type = 'log-posterior';
+            optionsMultistartMeigo.comp_type = 'sequential';
+            optionsMultistartMeigo.mode = 'silent';
+            optionsMultistartMeigo.localOptimizer = 'meigo-ess';
+            optionsMultistartMeigo.localOptimizerOptions = MeigoOptions;
+            optionsMultistartMeigo.n_starts = 2;
+            
+            parametersMeigo = getMultiStarts(testCase.parameters, testCase.objectiveFunction, optionsMultistartMeigo);
+            testCase.verifyMultiStartResults(parametersMeigo, optionsMultistartMeigo);
+        end
 
-            testCase.assertEqual(multiStartParams.MS.logPost, ones(numStarts, 1) * testCase.llh_true, 'RelTol', 1e-2)
-            testCase.assertEqual(multiStartParams.MS.par, repmat(testCase.theta_true, 1, numStarts), 'RelTol', 1e-1)
-        end        
+        function testSamplingSingleChainAM(testCase)
+            optionsMultistart = PestoOptions();
+            optionsMultistart.obj_type = 'log-posterior';
+            optionsMultistart.n_starts = 2;
+            optionsMultistart.comp_type = 'sequential';
+            optionsMultistart.mode = 'silent';
+
+            optionsMultistart.MCMC.sampling_scheme = 'single-chain';
+            optionsMultistart.SC.proposal_scheme   = 'AM';
+            optionsMultistart.MCMC.nsimu_warmup    = 2e2;
+            optionsMultistart.MCMC.thinning        = 10;
+            optionsMultistart.MCMC.nsimu_run       = 2e3;
+            
+            testCase.parameters.user.theta_0 = testCase.theta_true;
+            testCase.parameters.user.Sigma_0 = diag([1,1]);
+            
+            parameters = getParameterSamples(testCase.parameters, testCase.objectiveFunction, optionsMultistart);
+        end
+        
+        function testConfidenceIntervals(testCase)
+            
+            options = PestoOptions();
+            options.obj_type = 'log-posterior';
+            options.n_starts = 1;
+            options.comp_type = 'sequential';
+            options.mode = 'silent';
+            
+            multiStartParams = getMultiStarts(testCase.parameters, testCase.objectiveFunction, options);
+            
+            alpha = [0.9, 0.95, 0.99];
+            multiStartParams = getParameterConfidenceIntervals(multiStartParams, alpha, options);
+        end
+    end
+    
+    methods
+        function verifyMultiStartResults(testCase, multiStartParams, optionsMultistart)
+            numStarts = optionsMultistart.n_starts;
+            numParams = testCase.parameters.number;
+            
+            testCase.assertEqual(multiStartParams.MS.n_starts, optionsMultistart.n_starts, 'Incorrect number of starts');
+            
+            testCase.verifySize(multiStartParams.MS.par0, [numParams, numStarts], 'Error in par0 dimensions.');
+            testCase.verifySize(multiStartParams.MS.par, [numParams, numStarts], 'Error in par dimensions.');
+            testCase.verifySize(multiStartParams.MS.logPost0, [numStarts, 1], 'Error in logPost0 dimensions.');
+            testCase.verifySize(multiStartParams.MS.logPost, [numStarts, 1], 'Error in logPost dimensions.');
+            testCase.verifySize(multiStartParams.MS.gradient, [numParams, numStarts], 'Error in gradient dimensions.');
+            testCase.verifySize(multiStartParams.MS.n_objfun, [numStarts, 1], 'Error in n_objfun dimensions.');
+            testCase.verifySize(multiStartParams.MS.n_iter, [numStarts, 1], 'Error in n_iter dimensions.');
+            testCase.verifySize(multiStartParams.MS.t_cpu, [numStarts, 1], 'Error in t_cpu dimensions.');
+            testCase.verifySize(multiStartParams.MS.exitflag, [numStarts, 1], 'Error in exitflag dimensions.');
+            
+            if numStarts == 1
+                testCase.verifySize(multiStartParams.MS.hessian, [numParams, numParams], 'Error in hessian dimensions.');
+            else
+                testCase.verifySize(multiStartParams.MS.hessian, [numParams, numParams, numStarts], 'Error in hessian dimensions.');
+            end
+
+            testCase.assertEqual(multiStartParams.MS.logPost, ones(numStarts, 1) * testCase.llh_true, 'RelTol', 1e-2, 'Incorrect optimal log likelihood')
+            testCase.assertEqual(multiStartParams.MS.par, repmat(testCase.theta_true, 1, numStarts), 'RelTol', 1e-1, 'Incorrect optimal parameters.')
+            
+        end
     end
 end
