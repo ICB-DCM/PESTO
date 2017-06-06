439796fd1e1a4eaabf0ffa6727e1cd8c5df0bce9
Benjamin <benjamin.ballnus@gmail.com>
2017-06-06 13:27:12 +0200

    Added Banana RBPT Example

--- examples/RBPT_Banana/mainExampleBanana.m
+++ examples/RBPT_Banana/mainExampleBanana.m
@@ -1,23 +1,40 @@
+% Main file of the banana example for RBPT debugging
+%
+% Demonstrates the use of:
+% * getParameterSamples()
+%
+% Demonstrates furthermore:
+% * how to use RBPT
+% * how to use advanced plotting tools from PESTO
+%
+% The example problem is a mixture of Gaussian modes in the first two
+% dimensions and a single mode in any other dimension. The dimensionality,
+% angle, scaling and position of modes can be altered in defineGaussLLH.m.
+% This example was written by B. Ballnus.
+% 
+% Single-chain and multi-chain Monte-Carlo sampling is performed by 
+% getParameterSamples() and with different settings (commented code 
+% version) and plotted in 1D and 2D.
+
+
+
 %% Preliminary
 clear all
 close all
 clc
 
+
 %% Problem initialization
 
 % Settings for this example
-radius = 100;
-sigma = 2;
-dimi = 18; % extraDimensions
-logP = @(theta) simulateRingLLH(theta, radius, sigma, dimi);
-ringDimension = 2;
+logP = @(theta) bananaLLH(theta);
 
 % Set required sampling options for Parallel Tempering
-par.number = ringDimension + dimi;
-par.min    = [-200;-200;-200*ones(dimi,1)];
-par.max    = [200;200;200*ones(dimi,1)];
+par.number = 3;
+par.min    = [0;0;1e-3];
+par.max    = [5;5;1e2];
 par.name   = {};
-for i = 1 : dimi + 2
+for i = 1 : 3
    par.name{end+1} = ['\theta_' num2str(i)];
 end
 
@@ -45,7 +62,7 @@ options.mode                = 'text';
 
 % Using RBPT
 options.samplingAlgorithm     = 'RBPT';
-options.RBPT.nTemps           = 30;
+options.RBPT.nTemps           = 10;
 options.RBPT.exponentT        = 1000;   
 options.RBPT.maxT             = 2000;
 options.RBPT.alpha            = 0.51;
@@ -59,7 +76,7 @@ options.RBPT.trainPhaseFrac   = 0.1;
 options.RBPT.RPOpt.rng                  = 7;
 options.RBPT.RPOpt.nSample              = floor(options.nIterations*options.RBPT.trainPhaseFrac)-1;
 options.RBPT.RPOpt.crossValFraction     = 0.2;
-options.RBPT.RPOpt.modeNumberCandidates = [1,2,3,4,5];
+options.RBPT.RPOpt.modeNumberCandidates = [1,2,3,4,5,6,7,8];
 options.RBPT.RPOpt.displayMode          = 'visual';
 options.RBPT.RPOpt.maxEMiterations      = 100;
 options.RBPT.RPOpt.nDim                 = par.number;
@@ -69,15 +86,10 @@ options.RBPT.RPOpt.upperBound           = par.max;
 options.RBPT.RPOpt.tolMu                = 1e-4 * (par.max(1)-par.min(1));
 options.RBPT.RPOpt.tolSigma             = 1e-2 * (par.max(1)-par.min(1));
 options.RBPT.RPOpt.dimensionsToPlot     = [1,2];
-options.RBPT.RPOpt.isInformative        = [1,1,zeros(1,options.RBPT.RPOpt.nDim-1)];
+options.RBPT.RPOpt.isInformative        = [1,1,1];
 
-
-randoms                     = randn(2,options.RBPT.nTemps);
-squareSum                   = sqrt(sum(randoms.^2));
-randomPointsOnRing          = randoms ./ squareSum;
-% plot(randomPointsOnRing(1,:),randomPointsOnRing(2,:),'.')
-options.theta0              = [radius*randomPointsOnRing;zeros(dimi,options.RBPT.nTemps)];
-options.sigma0              = 1e6*diag(ones(1,dimi+2));
+options.theta0              = par.min + (par.max-par.min).*rand(3,options.RBPT.nTemps);
+options.sigma0              = 1e6*diag(ones(1,3));
 
 % Perform the parameter estimation via sampling
 par = getParameterSamples(par, logP, options);
