e36b9f5108ccd6f669569202b75bd2f7d13f8c6b
Carolin Loos <carolin.loos@helmholtz-muenchen.de>
2018-05-08 09:29:01 +0200

    main files clean up

---
+++ examples/HierarchicalOptimization_examples/SmallJakStat/main_SmallJakStat.m
@@ -0,0 +1,35 @@
+% Main file of the JakStat signaling I example.
+%
+% Demonstrates the use of:
+% * logLikelhoodHierarchical()
+%
+% Performs parameter estimation for the standard and hierarchical approach
+% and Gaussian and Laplace noise.
+
+clear all 
+close all
+clc
+
+%% Compilation of simulation files using AMICI
+compilation_JakStat
+
+%% Optimization using fmincon
+runEstimation_JakStat('hierarchical','normal')
+runEstimation_JakStat('hierarchical','laplace')
+
+runEstimation_JakStat('standard','normal')
+runEstimation_JakStat('standard','laplace')
+
+%% Profile calculation
+runProfiles_JakStat('standard','normal')
+runProfiles_JakStat('standard','laplace')
+
+runProfiles_JakStat('hierarchical','normal')
+runProfiles_JakStat('hierarchical','laplace')
+
+%% Optimization using PSwarm
+runEstimation_JakStat('hierarchical','normal','pswarm')
+runEstimation_JakStat('hierarchical','laplace','pswarm')
+
+runEstimation_JakStat('standard','normal','pswarm')
+runEstimation_JakStat('standard','laplace','pswarm')
